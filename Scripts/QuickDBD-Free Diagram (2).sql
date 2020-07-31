26bc38781083b34ed1644dc6215e5dcd2e444665
atecza <agetecza@gmail.com>
2020-07-31 11:40:45 -0600

    finish API creation

---
+++ Scripts/QuickDBD-Free Diagram (2).sql
@@ -0,0 +1,63 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/bewOyV
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "States" (
+    "Country" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_States" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "EnvData" (
+    "Country" VARCHAR   NOT NULL,
+    "Region" VARCHAR   NOT NULL,
+    "HDI" FLOAT   NOT NULL,
+    "Footprint_Crop" FLOAT   NOT NULL,
+    "Footprint_Graze" FLOAT   NOT NULL,
+    "Footprint_Forest" FLOAT   NOT NULL,
+    "Footprint_Carbon" FLOAT   NOT NULL,
+    "Footprint_Fish" FLOAT   NOT NULL,
+    "Footprint_Total" FLOAT   NOT NULL,
+    "Land_Crop" FLOAT   NOT NULL,
+    "Land_Grazing" FLOAT   NOT NULL,
+    "Land_Forest" FLOAT   NOT NULL,
+    "Water_Fishing" FLOAT   NOT NULL,
+    "Land_Urban" FLOAT   NOT NULL,
+    "Threat_Species" FLOAT   NOT NULL,
+    "Emissions_CO2" BIGINT   NOT NULL,
+    "Biocapacity_Total" FLOAT   NOT NULL,
+    "BioCap_RD" INT   NOT NULL,
+    "Earths_Req" INT   NOT NULL,
+    "Countries_Req" INT   NOT NULL,
+    "Data_Quality" INT   NOT NULL
+);
+
+CREATE TABLE "CountryData" (
+    "Country" VARCHAR   NOT NULL,
+    "Surface_Area" BIGINT   NOT NULL,
+    "Population" BIGINT   NOT NULL,
+    "PopDensity" FLOAT   NOT NULL,
+    "PopGrowth" FLOAT   NOT NULL,
+    "PopUrban" FLOAT   NOT NULL,
+    "GDP" BIGINT   NOT NULL,
+    "GDPpercap" FLOAT   NOT NULL,
+    "GDPGrowth" INT   NOT NULL,
+    "EconAg" FLOAT   NOT NULL,
+    "EconInd" FLOAT   NOT NULL,
+    "EconService" FLOAT   NOT NULL,
+    "Unemployment" FLOAT   NOT NULL,
+    "Export" BIGINT   NOT NULL,
+    "IMPORT" BIGINT   NOT NULL,
+    "Gov_Education" FLOAT   NOT NULL,
+    "Women_Parliment" FLOAT   NOT NULL,
+    "Energy_Prod" INT   NOT NULL
+);
+
+ALTER TABLE "EnvData" ADD CONSTRAINT "fk_EnvData_Country" FOREIGN KEY("Country")
+REFERENCES "States" ("Country");
+
+ALTER TABLE "CountryData" ADD CONSTRAINT "fk_CountryData_Country" FOREIGN KEY("Country")
+REFERENCES "States" ("Country");
+
---
+++ app.py
@@ -0,0 +1,155 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import psycopg2
+
+from flask import Flask, jsonify
+import datetime as dt
+
+#make sure you have your own config on your computer in the SQL folder
+from config import key
+
+#################################################
+# Database Setup
+#################################################
+pg_user = 'postgres'
+pg_pwd = key
+pg_port = "5432"
+
+#remember to make this db in pgAdmin before
+# run Final_Query.sql in pgAdmin to create the tables
+
+database = 'Project2'
+url = f"postgresql://{pg_user}:{pg_pwd}@localhost:{pg_port}/{database}"
+
+engine = create_engine(f'{url}')
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+EnvironmentData = Base.classes.envdata
+CountryData = Base.classes.countrydata
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/EnvData<br/>"
+
+    )
+
+@app.route("/api/v1.0/EnvData")
+def EnvData():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    #make all the empty lists
+    Countries = []
+    HDI = []
+    FC = []
+    FG = []
+    FF = []
+    FCar = []
+    FFish = []
+    FT = []
+    LU = []
+    ECO2 = []
+    DQ = []
+    SurArea = []
+    Pop = []
+    PopD = []
+    PopGr = []
+    PopUrb = []
+    GDP = []
+    GDPGrowth = []
+    EconAg = []
+    EconInd = []
+    EconServ = []
+    GovEd = []
+    WP = []
+    
+    EData = session.query(EnvironmentData).all()
+    
+    for x in EData:
+        Countries.append(x.Country)
+        HDI.append(x.HDI)
+        FC.append(x.Footprint_Crop)
+        FG.append(x.Footprint_Graze)
+        FF.append(x.Footprint_Forest)
+        FCar.append(x.Footprint_Carbon)
+        FFish.append(x.Footprint_Fish)
+        FT.append(x.Footprint_Total)
+        LU.append(x.Land_Urban)
+        ECO2.append(x.Emissions_CO2)
+        DQ.append(x.Data_Quality)
+    
+    CData = EData = session.query(CountryData).all()
+    
+    for x in CData:
+        SurArea.append(x.Surface_Area)
+        Pop.append(x.Population)
+        PopD.append(x.PopDensity)
+        PopGr.append(x.PopGrowth)
+        PopUrb.append(x.PopUrban)
+        EconAg.append(x.EconAg)
+        EconInd.append(x.EconInd)
+        EconServ.append(x.EconService)
+        GovEd.append(x.Gov_Education)
+        WP.append(x.Women_Parliment)
+    
+    #put everything into a dictionary
+    full_data = {
+        "Country":Countries,
+        "HDI":HDI,
+        "Footprint_Crop":FC,
+        "Footprint_Graze":FG,
+        "Footprint_Forest":FF,
+        "Footprint_Carbon":FCar,
+        "Footprint_Fish":FF,
+        "Footprint_Total":FT,
+        "Land_Urban":LU,
+        "Emissions_CO2":ECO2,
+        "Data_Quality":DQ,
+        "Area":SurArea,
+        "Population":Pop,
+        "PopDensity":PopD,
+        "PopGrowth":PopGr,
+        "PopUrban":PopUrb,
+        "EconAg":EconAg,
+        "EconInd":EconInd,
+        "EconServ":EconServ,
+        "GovEducation":GovEd,
+        "Women_Parliment":WP
+    
+    }
+
+    #Create my main dictionary
+    
+    myData = {"Data":full_data}
+    
+    """Return the JSON representation of your dictionary"""
+    return (jsonify(myData))
+    
+    session.close()
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
