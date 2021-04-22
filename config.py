839574f9714a36e09688c8087d6be06361a67e73
atecza <agetecza@gmail.com>
2021-04-22 16:17:52 -0600

    take out some files for gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-config_dict = {
-    'key':'postgres',
-    'database': 'd5l7n5pdmr2nb8',
-    'pg_user':'rzhyzfzehkykya',
-    'pg_port':'5432',
-    'pg_pwd':'5acbb2ab0883921e445a6bd7f152fd4b0bcbaadee573cba83d881ecbbce91ea5'}
